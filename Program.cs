1369ef5d048789fc78364b1502d92ab26c72c7a9
KissAdambat <kissa@kkszki.hu>
2025-09-10 09:24:51 +0200

    feladat3 ready

--- Program.cs
+++ Program.cs
@@ -12,7 +12,18 @@ namespace OOPgyak
         private string nev;
         private int kor;
         
-        public Szemely(string Nev, int Kor)
+        public string Nev
+        {
+            get { return nev; }
+            set { nev = value; }
+        }
+
+        public int Kor
+        {
+            get { return kor; }
+            set { kor = value; }
+        }
+        /*public Szemely(string Nev, int Kor)
         {
             nev = Nev;
             kor = Kor;
@@ -20,16 +31,21 @@ namespace OOPgyak
         public string Kiir()
         {
             return $"A tanulo neve: {nev} és {kor} éves";
-        }
+        }*/
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Szemely tan1 = new Szemely("Jani",55);
+            Szemely tan1 = new Szemely();
+            tan1.Nev = "Jani";
+            Console.Write(tan1.Nev);
+            tan1.Kor = 11;
+            Console.Write(tan1.Kor);
+            /*Szemely tan1 = new Szemely("Jani",55);
             Console.WriteLine(tan1.Kiir());
             Szemely tan2 = new Szemely("Peti",22);
-            Console.WriteLine(tan2.Kiir());
+            Console.WriteLine(tan2.Kiir());*/
         }
     }
 }
