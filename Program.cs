37a8bac37a3c5d2d16d3335ad6801922d68a91a8
KissAdambat <kissa@kkszki.hu>
2025-09-10 08:05:20 +0200

    oop project is created.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPgyak
+{
+    class Szemely
+    {
+        public string Nev { get; set; }
+        public int Kor { get; set; }
+        public Szemely(string nev, int kor)
+        {
+            Nev = nev;
+            Kor = kor;
+        }
+        public void Koszones()
+        {
+            Console.WriteLine($"Szia, {Nev} vagyok, {Kor} eves.");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
